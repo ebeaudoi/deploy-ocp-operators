5f79cb0826792b8db3e61489f0849921cf3b34ee
ebeaudoi <ebeaudoi@redhat.com>
2024-01-08 10:34:05 -0500

    updated rhacm

--- rhacm/update-rhacm-default-values.sh
+++ rhacm/update-rhacm-default-values.sh
@@ -4,22 +4,48 @@
 # IMPORTANT                    #
 # Update the 3 below variables #
 RHCAM_CHANNEL="release-2.8"
-RHCAM_CATALOG="my-redhat-v413-catalog"
-RHACM_OSE_CLI="ebdnquay.ebdnlab.vmware.tamlab.rdu2.redhat.com:8443/redhat-v413/openshift4/ose-cli:e11912cb"
+RHCAM_CATALOG="cs-my-redhat-catalog"
+RHACM_OSE_CLI="quay.devu.ca:8443/redhat-v414/openshift4/ose-cli:e11912cb"
+
+# Backup the files
+cp operator/overlay/airgapped/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
+cp observability/overlay/airgapped/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
+cp instance/overlay/airgapped/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
+OSE_CLI=$( echo $RHACM_OSE_CLI|sed 's/\//\\\//g')
+
+# Showing new values
+echo "-----------------------------"
+echo "Channel = $RHCAM_CHANNEL"
+echo "Catalog = $RHCAM_CATALOG"
+echo "OSE CLI = $OSE_CLI"
+echo "-----------------------------"
 
 #####################################
 # Update operator overlay yaml file #
   # Update the subscription channel
-sed "/path:\ \/spec\/channel/{ n; s/value: .*$/value: $RHCAM_CHANNEL/g }" operator/overlays/airgapped/kustomization.yaml
+echo
+echo "-- Update operator channel-- "
+echo "File: operator/overlay/airgapped/kustomization.yaml"
+echo
+sed -i "/path:\ \/spec\/channel/{ n; s/value: .*$/value: $RHCAM_CHANNEL/g }" operator/overlay/airgapped/kustomization.yaml
   # Update the subscription catalog
-sed "/path:\ \/spec\/source/{ n; s/value: .*$/value: $RHCAM_CATALOG/g }" operator/overlays/airgapped/kustomization.yaml
+echo "-- Update operator catalog --"
+echo "File: operator/overlay/airgapped/kustomization.yaml"
+echo ""
+sed -i "/path:\ \/spec\/source/{ n; s/value: .*$/value: $RHCAM_CATALOG/g }" operator/overlay/airgapped/kustomization.yaml
 
 ##########################################
 # Update Observability overlay yaml file #
   # Update the ose-cli from the private quay
-sed "/path:\ \/spec\/template\/spec\/containers\/0\/image/{ n; s/value: .*$/value: $RHACM_OSE_CLI/g }" observability/overlay/airgapped/kustomization.yaml
+echo "-- Update ose cli image --"
+echo "File: observability/overlay/airgapped/kustomization.yaml"
+echo ""
+sed -i "/path:\ \/spec\/template\/spec\/containers\/0\/image/{ n; s/value: .*$/value: $OSE_CLI/g }" observability/overlay/airgapped/kustomization.yaml
 
 ######################################################
 # Update MultiClusterHub overlay yaml file           #
   # Update the  mce-subscription-spec with RH catalog
-sed "/path:\ \/metadata\/annotations\/installer\.open-cluster-management\.io\/mce-subscription-spec/{ n; s/value: .*$/value:\ \'\{\"source\": \"$RHCAM_CATALOG\"\}\'/g }" instance/overlay/airgapped/kustomization.yaml
+echo "-- Update instance --"
+echo "File: instance/overlay/airgapped/kustomization.yaml"
+echo ""
+sed -i "/path:\ \/metadata\/annotations\/installer\.open-cluster-management\.io\/mce-subscription-spec/{ n; s/value: .*$/value:\ \'\{\"source\": \"$RHCAM_CATALOG\"\}\'/g }" instance/overlay/airgapped/kustomization.yaml
