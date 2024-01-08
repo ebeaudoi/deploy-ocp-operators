405e7babad6f77541407451dedb49b46ac4f3e01
ebeaudoi <ebeaudoi@redhat.com>
2024-01-08 10:02:13 -0500

    Update update-odf-default-values.sh

--- odf/update-odf-default-values.sh
+++ odf/update-odf-default-values.sh
@@ -3,10 +3,10 @@
 ################################
 # IMPORTANT                    #
 # Update the 3 below variables #
-CHANNEL="EBDNstable-4.14"
-CATALOG="EBDNcs-my-redhat-catalog"
-OSE_CLI_IMAGE="EBDNquay.devu.ca:8443/redhat-v414/openshift4/ose-cli:e11912cb"
-CAPACITY="eBDN20Gi"
+CHANNEL="stable-4.14"
+CATALOG="cs-my-redhat-catalog"
+OSE_CLI_IMAGE="quay.devu.ca:8443/redhat-v414/openshift4/ose-cli:e11912cb"
+CAPACITY="150Gi"
 
 # Backup the files
 cp operator/overlay/airgapped/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
@@ -25,19 +25,22 @@ echo "-----------------------------"
   # Update the subscription channel
 echo
 echo "-- Update operator channel-- "
-echo ""
+echo "File: operator/overlay/airgapped/kustomization.yaml"
+echo
 sed -i "/path:\ \/spec\/channel/{ n; s/value: .*$/value: $CHANNEL/g }" operator/overlay/airgapped/kustomization.yaml
   # Update the subscription catalog
 echo "-- Update operator catalog --"
+echo "File: operator/overlay/airgapped/kustomization.yaml"
 echo ""
 sed -i "/path:\ \/spec\/source/{ n; s/value: .*$/value: $CATALOG/g }" operator/overlay/airgapped/kustomization.yaml
   # Update image to enable console
 echo "-- Update ose cli image --"
+echo "File: operator/overlay/airgapped/kustomization.yaml"
 echo ""
 sed -i "/path:\ \/spec\/template\/spec\/containers\/0\/image/{ n; s/value: .*$/value: $OSE_CLI/g }" operator/overlay/airgapped/kustomization.yaml
 
 ####################################
 # Update Stroage capacity          #
 echo "-- Update storage capacity --"
-echo ""
+echo "File: noobaa/overlay/default/patch-storage-capacity.yaml"
 sed -i "/path:\ \/spec\/pvPool\/resources\/requests\/storage/{ n; s/value: .*$/value: $CAPACITY/g }" noobaa/overlay/default/patch-storage-capacity.yaml
