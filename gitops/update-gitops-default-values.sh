40514b0c565434631fa9a2b1499aa70178ae4aaa
admin <admin@bastion.ebdnlab.vmware.tamlab.rdu2.redhat.com>
2024-11-28 12:41:20 -0500

    changed gitops

--- gitops/update-gitops-default-values.sh
+++ gitops/update-gitops-default-values.sh
@@ -4,16 +4,17 @@
 # IMPORTANT                    #
 # Update the 3 below variables #
 CHANNEL="latest"
-CATALOG="cs-my-redhat-catalog"
-
+CATALOG="redhat-operators"
+#GITUSER="admin"
 # Backup the files
-cp operator/overlays/airgapped/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
-cp instance/overlays/airgapped/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
-
+cp operator/overlays/infra/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
+cp instance/overlays/infra/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
+cp example-instance/overlays/airgapped/kustomization.yaml{,.$(date +%Y%m%d-%HH%M)}
 # Showing new values
 echo "-----------------------------"
 echo "Channel = $CHANNEL"
 echo "Catalog = $CATALOG"
+#echo "Git User = $GITUSER"
 echo "-----------------------------"
 
 #####################################
@@ -21,12 +22,18 @@ echo "-----------------------------"
   # Update the subscription channel
 echo
 echo "-- Update operator channel-- "
-echo "File: operator/overlays/airgapped/kustomization.yaml"
+echo "File: operator/overlays/infra/kustomization.yaml"
 echo
-sed -i "/path:\ \/spec\/channel/{ n; s/value: .*$/value: $CHANNEL/g }" operator/overlays/airgapped/kustomization.yaml
+sed -i "/path:\ \/spec\/channel/{ n; s/value: .*$/value: $CHANNEL/g }" operator/overlays/infra/kustomization.yaml
   # Update the subscription catalog
 echo "-- Update operator catalog --"
-echo "File: operator/overlays/airgapped/kustomization.yaml"
+echo "File: operator/overlays/infra/kustomization.yaml"
 echo ""
-sed -i "/path:\ \/spec\/source/{ n; s/value: .*$/value: $CATALOG/g }" operator/overlays/airgapped/kustomization.yaml
+sed -i "/path:\ \/spec\/source/{ n; s/value: .*$/value: $CATALOG/g }" operator/overlays/infra/kustomization.yaml
+
+# Update gitops instance
+  # Add users
+#echo "-- Update instance users --"
+#echo "File: example-instance/overlays/airgapped/kustomization.yaml"
+#sed -i "/path:\ \/users/{ n; s/value: .*$/value: $GITUSER/g }" example-instance/overlays/airgapped/kustomization.yaml
 
